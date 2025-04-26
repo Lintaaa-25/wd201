997c205db1ede3cd3f0bc7677377e56ea389f726
Lintaaa-25 <lintababu3@gmail.com>
2025-04-26 10:39:38 +0530

    Update app.js

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const path = require('path');
 const app = express();
 
@@ -20,3 +20,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+module.exports = app;
