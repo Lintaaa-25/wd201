59767c885d2aa9fec0da842d4a897ba2d025d786
Lintaaa-25 <lintababu3@gmail.com>
2025-04-29 20:10:02 +0530

    Update app.js

--- todo-app/app.js
+++ todo-app/app.js
@@ -5,12 +5,28 @@ const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const csrf = require("tiny-csrf");
-const secret = "e34f8c1f5b5d8f7b3947a2f013529fd5"; 
+
+const secret = "e34f8c1f5b5d8f7b3947a2f013529fd5";
+
+// Trust proxy for secure cookies (Render uses reverse proxy)
+app.set("trust proxy", 1);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
 app.use(cookieParser(secret));
-app.use(csrf(secret, ["POST", "PUT", "DELETE"]));
+
+
+  csrf(secret, {
+    cookie: {
+      httpOnly: true,
+      sameSite: "lax", 
+      secure: true,    
+    },
+  })
+);
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
@@ -46,15 +62,12 @@ app.post("/todos", async (req, res) => {
 
 app.put("/todos/:id", async (req, res) => {
   try {
-    console.log("PUT body:",req.body);
+    console.log("PUT body:", req.body);
     const { completed } = req.body;
     const todo = await Todo.setCompletionStatus(req.params.id, completed);
-
     if (todo[0] === 0) {
-      // If no rows were updated, return an error
       return res.status(404).send("Todo not found");
     }
-
     res.json({ success: true });
   } catch (err) {
     console.error(err);
@@ -62,11 +75,9 @@ app.put("/todos/:id", async (req, res) => {
   }
 });
 
-
-
 app.delete("/todos/:id", async (req, res) => {
   try {
-    console.log("DELETE todo:",req.params.id);
+    console.log("DELETE todo:", req.params.id);
     const deleted = await Todo.remove(req.params.id);
     res.json({ success: deleted ? true : false });
   } catch (err) {
