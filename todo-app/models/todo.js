01e10a5a410980e28326cd5e80c746d2423626dc
Lintaaa-25 <lintababu3@gmail.com>
2025-04-29 22:18:17 +0530

    Update todo.js

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -1,94 +1,35 @@
 "use strict";
-const { Model, Op } = require("sequelize");
+const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
-  class Todo extends Model {
-    // Adds a new Todo item
-    static async addTodo({ title, dueDate }) {
-      if (!title || !dueDate) throw new Error("Title and DueDate are required");
-      return await Todo.create({ title, dueDate, completed: false });
-    }
+  class Todo extends Model {}
 
-    // Fetches all Todos: overdue, due today, due later, and completed
-    static async getTodos() {
-      const today = new Date().toISOString().split("T")[0];
-
-      const overdue = await Todo.findAll({
-        where: {
-          dueDate: { [Op.lt]: today },
-          completed: false,
-        },
-        order: [["dueDate", "ASC"]],
-      });
-
-      const dueToday = await Todo.findAll({
-        where: {
-          dueDate: { [Op.eq]: today },
-          completed: false,
-        },
-        order: [["dueDate", "ASC"]],
-      });
-
-      const dueLater = await Todo.findAll({
-        where: {
-          dueDate: { [Op.gt]: today },
-          completed: false,
-        },
-        order: [["dueDate", "ASC"]],
-      });
-
-      const completedItems = await Todo.findAll({
-        where: {
-          completed: true,
-        },
-        order: [["dueDate", "ASC"]],
-      });
-
-      return { overdue, dueToday, dueLater, completedItems };
-    }
-
-    // Removes a Todo by ID
-    static async remove(id) {
-      return await Todo.destroy({ where: { id } });
-    }
-
-    // Updates a Todo item with the provided fields
-    static async updateTodo(id, updatedFields) {
-      return await Todo.update(updatedFields, { where: { id } });
-    }
-
-    // Updates the completion status of a Todo item
-    static async setCompletionStatus(id, completed) {
-      if (typeof completed !== 'boolean') {
-        throw new Error('Completed status must be a boolean');
-      }
-      return await Todo.update(
-        { completed },
-        { where: { id } }
-      );
-    }
-  }
-
-  // Initialize the Todo model with validations and default values
   Todo.init(
     {
       title: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          notEmpty: true,
+          notEmpty: {
+            msg: "Title cannot be empty",
+          },
         },
       },
       dueDate: {
         type: DataTypes.DATEONLY,
         allowNull: false,
         validate: {
-          notEmpty: true,
+          notEmpty: {
+            msg: "Due date is required",
+          },
+          isDate: {
+            msg: "Must be a valid date",
+          },
         },
       },
       completed: {
         type: DataTypes.BOOLEAN,
-        defaultValue: false, // Default to false (incomplete)
+        defaultValue: false,
       },
     },
     {
