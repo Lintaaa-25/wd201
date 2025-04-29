62995c3efb93de60ae950ce01cd3b55bd3611096
Lintaaa-25 <lintababu3@gmail.com>
2025-04-30 01:13:25 +0530

    Update index.js

--- todo-app/models/index.js
+++ todo-app/models/index.js
@@ -1,20 +1,9 @@
-// models/index.js
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
 const Sequelize = require("sequelize");
-const fs = require("fs");
-const path = require("path");
 
-const env = process.env.NODE_ENV || "development";  // ✅ Define env
-const config = require(__dirname + '/../config/config.json')[env];  // ✅ Now this works
-
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 const db = {};
-
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 db.Todo = require("./todo")(sequelize, Sequelize);
