4d2903f532e90f48bbc940fbbc153499fd5d080f
Lintaaa-25 <lintababu3@gmail.com>
2025-04-30 10:17:32 +0530

    Update todo.js

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -2,29 +2,44 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   const Todo = sequelize.define("Todo", {
-    title: DataTypes.STRING,
-    dueDate: DataTypes.DATEONLY,
-    completed: DataTypes.BOOLEAN,
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: "Title must not be empty" },
+      },
+    },
+    dueDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: "Due date is required" },
+        isDate: { msg: "Must be a valid date" },
+      },
+    },
+    completed: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
   });
 
+  // Method to update completion status
   Todo.prototype.setCompletionStatus = async function (status) {
     this.completed = status;
     await this.save();
   };
 
+  // Group todos into four categories
   Todo.groupTodos = async function () {
-    const todos = await Todo.findAll();
+    const todos = await Todo.findAll({ order: [["dueDate", "ASC"]] });
     const today = new Date().toISOString().split("T")[0];
-    const groups = { overdue: [], dueToday: [], dueLater: [], completed: [] };
 
-    todos.forEach((todo) => {
-      if (todo.completed) return groups.completed.push(todo);
-      if (todo.dueDate < today) groups.overdue.push(todo);
-      else if (todo.dueDate === today) groups.dueToday.push(todo);
-      else groups.dueLater.push(todo);
-    });
-
-    return groups;
+    return {
+      overdue: todos.filter((todo) => !todo.completed && todo.dueDate < today),
+      dueToday: todos.filter((todo) => !todo.completed && todo.dueDate === today),
+      dueLater: todos.filter((todo) => !todo.completed && todo.dueDate > today),
+      completedItems: todos.filter((todo) => todo.completed),
+    };
   };
 
   return Todo;
