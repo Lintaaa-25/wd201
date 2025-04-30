f19fbf9cada2d4ed298d3eb9c73ab648e8a39ec3
Lintaaa-25 <lintababu3@gmail.com>
2025-04-30 09:52:40 +0530

    Update index.js

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,10 +1,8 @@
 const app = require("./app");
-const db = require("./models");
+const { sequelize } = require("./models");
 
-const port = process.env.PORT || 3000;
-
-db.sequelize.sync().then(() => {
-  app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
-  });
-});
+(async () => {
+  await sequelize.sync();
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`Running on port ${port}`));
+})();
