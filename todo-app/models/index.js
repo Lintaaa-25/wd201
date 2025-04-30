a099347b49bbb75a016e3b6ac5ec91a12296abc6
Lintaaa-25 <lintababu3@gmail.com>
2025-04-30 09:53:08 +0530

    Update index.js

--- todo-app/models/index.js
+++ todo-app/models/index.js
@@ -1,17 +1,9 @@
-const Sequelize = require('sequelize');
-
-// Directly use DATABASE_URL from Render environment variable
-const config = process.env.DATABASE_URL || require(__dirname + '/../config/config.json')[process.env.NODE_ENV || 'development'];
-
-const sequelize = new Sequelize(config, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  logging: false, // Set to true if you want to see SQL queries in logs
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: process.env.DATABASE_URL.includes("sslmode=require")
+    ? { ssl: { require: true, rejectUnauthorized: false } }
+    : {},
 });
-
-const db = {};
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
-db.Todo = require("./todo")(sequelize, Sequelize);
-
-module.exports = db;
+const Todo = require("./todo")(sequelize);
+module.exports = { sequelize, Todo };
