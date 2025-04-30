90e8571ac107ef29081a0b3677e76bf97b006482
Lintaaa-25 <lintababu3@gmail.com>
2025-05-01 00:06:05 +0530

    Update index.js

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,6 +1,5 @@
 const app = require("./app");
-const port = process.env.PORT || 3000;
 
 app.listen(3000, () => {
-  console.log("Server started on http://localhost:3000");
+  console.log("Started express server at port 3000");
 });
