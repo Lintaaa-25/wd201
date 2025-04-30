d442390ae3bfb309cf772bf5fe66ebb2bc77998c
Lintaaa-25 <lintababu3@gmail.com>
2025-05-01 00:05:32 +0530

    Update app.js

--- todo-app/app.js
+++ todo-app/app.js
@@ -4,20 +4,19 @@ const path = require("path");
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const csrf = require("csurf");
+const csrf = require("tiny-csrf");
+const secret = "e34f8c1f5b5d8f7b3947a2f013529fd5"; 
 
-// Middleware setup
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cookieParser("w89R7nfpK2tUvXA3YeLg51BZqmJcHd6o"));
-
-// Set up CSRF protection
-app.use(csrf({ cookie: true }));
-
+app.use(cookieParser(secret));
+app.use(csrf(secret));
 app.use(express.static(path.join(__dirname, "public")));
+
 app.set("view engine", "ejs");
 
 // Routes
+
 app.get("/", async (req, res) => {
   const todos = await Todo.getTodos();
   res.render("index", {
@@ -31,12 +30,13 @@ app.get("/", async (req, res) => {
 
 app.post("/todos", async (req, res) => {
   try {
-    const { title, dueDate } = req.body;
-    if (!title || title.trim() === "" || !dueDate || dueDate.trim() === "") {
+    if (!req.body.title || !req.body.dueDate) {
       return res.status(400).send("Title and DueDate are required");
     }
-
-    await Todo.addTodo({ title, dueDate });
+    await Todo.addTodo({
+      title: req.body.title,
+      dueDate: req.body.dueDate,
+    });
     res.redirect("/");
   } catch (err) {
     console.error(err);
@@ -44,17 +44,24 @@ app.post("/todos", async (req, res) => {
   }
 });
 
-
 app.put("/todos/:id", async (req, res) => {
   try {
-    const todo = await Todo.findByPk(req.params.id);
-    const updated = await todo.setCompletionStatus(req.body.completed);
-    res.json(updated);
+    const { completed } = req.body;
+    const todo = await Todo.setCompletionStatus(req.params.id, completed);
+
+    if (todo[0] === 0) {
+      // If no rows were updated, return an error
+      return res.status(404).send("Todo not found");
+    }
+
+    res.json({ success: true });
   } catch (err) {
     console.error(err);
-    res.status(422).send(err.message);
+    res.status(500).send(err.message);
   }
 });
+
+
 
 app.delete("/todos/:id", async (req, res) => {
   try {
