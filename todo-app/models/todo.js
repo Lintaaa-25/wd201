b118f69f9a839ad5e565ac83847972b6d508b26b
Lintaaa-25 <lintababu3@gmail.com>
2025-05-04 09:14:14 +0530

    Update todo.js

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -1,57 +1,50 @@
-'use strict';
-const { Model } = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Todo extends Model {
-    static associate(models) {}
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../db");
 
-    static async addTodo({ title, dueDate }) {
-      return await this.create({ title, dueDate, completed: false });
-    }
+class Todo extends Model {
+  static async getTodos() {
+    return await Todo.findAll({ order: [["dueDate", "ASC"]] });
+  }
 
-    static async setCompletionStatus(id, completed) {
-      const todo = await this.findByPk(id);
-      if (!todo) throw new Error('Todo not found');
-      return await todo.update({ completed });
-    }
+  static async addTodo({ title, dueDate, completed }) {
+    return await Todo.create({ title, dueDate, completed });
+  }
 
-    static async deleteTodo(id) {
-      return await this.destroy({ where: { id } });
-    }
-
-    static async getTodos() {
-      const today = new Date().toISOString().split('T')[0];
-      const allTodos = await this.findAll({ order: [['id', 'ASC']] });
-
-      return {
-        overdue: allTodos.filter(todo => todo.dueDate < today && !todo.completed),
-        dueToday: allTodos.filter(todo => todo.dueDate === today && !todo.completed),
-        dueLater: allTodos.filter(todo => todo.dueDate > today && !todo.completed),
-        completed: allTodos.filter(todo => todo.completed)
-      };
+  static async deleteTodo(id) {
+    const todo = await Todo.findByPk(id);
+    if (todo) {
+      await todo.destroy();
     }
   }
 
-  Todo.init(
-    {
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: { notEmpty: true }
-      },
-      dueDate: {
-        type: DataTypes.DATEONLY,
-        allowNull: false,
-        validate: { notEmpty: true }
-      },
-      completed: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
-      }
-    },
-    {
-      sequelize,
-      modelName: 'Todo'
+  static async setCompletionStatus(id, completed) {
+    const todo = await Todo.findByPk(id);
+    if (todo) {
+      todo.completed = completed;
+      await todo.save();
     }
-  );
-  return Todo;
-};
+  }
+}
+
+Todo.init(
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dueDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    completed: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Todo",
+  }
+);
+
+module.exports = Todo;
