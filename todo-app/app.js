083c8f37f2084f736fbd357c9ac4263ec6a86fc6
Lintaaa-25 <lintababu3@gmail.com>
2025-05-11 23:33:05 +0530

    Update app.js

--- todo-app/app.js
+++ todo-app/app.js
@@ -16,6 +16,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
 // Routes
+
 app.get("/", async (req, res) => {
   const todos = await Todo.getTodos();
   res.render("index", {
@@ -47,10 +48,6 @@ app.put("/todos/:id/markAsCompleted", async (req, res) => {
   console.log("We have to update a todo with ID:", req.params.id);
   const todo = await Todo.findByPk(req.params.id);
   try {
-    if (req.body._csrf !== req.csrfToken()) {
-      return res.status(403).json({ error: "Invalid CSRF token" });
-    }
-
     if (todo) {
       const updatedTodo = await todo.markAsCompleted();
       return res.json(updatedTodo);
@@ -67,10 +64,6 @@ app.put("/todos/:id/markAsIncompleted", async (req, res) => {
   console.log("We have to mark a todo as incompleted with ID:", req.params.id);
   const todo = await Todo.findByPk(req.params.id);
   try {
-    if (req.body._csrf !== req.csrfToken()) {
-      return res.status(403).json({ error: "Invalid CSRF token" });
-    }
-
     if (todo) {
       const updatedTodo = await todo.markAsIncompleted();
       return res.json(updatedTodo);
@@ -85,10 +78,6 @@ app.put("/todos/:id/markAsIncompleted", async (req, res) => {
 
 app.delete("/todos/:id", async (req, res) => {
   try {
-    if (req.body._csrf !== req.csrfToken()) {
-      return res.status(403).json({ error: "Invalid CSRF token" });
-    }
-
     const deleted = await Todo.remove(req.params.id);
     res.json({ success: deleted ? true : false });
   } catch (err) {
@@ -98,3 +87,16 @@ app.delete("/todos/:id", async (req, res) => {
 });
 
 module.exports = app;
+index.js
+
+const app = require("./app");
+const { sequelize } = require("./models");
+
+const port = process.env.PORT || 3000;
+
+sequelize.sync().then(() => {
+  app.listen(port, () => {
+    console.log(Server running on http://localhost:${port});
+  });
+});
+
