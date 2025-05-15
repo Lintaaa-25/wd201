26745937e38fc50aae0ef2d8d6031284a80769da
Lintaaa-25 <lintababu3@gmail.com>
2025-05-16 01:11:11 +0530

    Update app.js

--- todo-app/app.js
+++ todo-app/app.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 
-// Routes
+
 
 app.get("/", async (req, res) => {
 const todos = await Todo.getTodos();
