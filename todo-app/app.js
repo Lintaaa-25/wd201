9486b4b0b8133fce4a955b237efd781977beffe8
Lintaaa-25 <lintababu3@gmail.com>
2025-05-15 23:52:27 +0530

    Update app.js

--- todo-app/app.js
+++ todo-app/app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser(secret));
 app.use(csrf(secret));
-app.use(express.static(path.join(\_\_dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 
