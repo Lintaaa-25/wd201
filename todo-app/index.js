2c77b96031c9123f74a89bc0fef020f386487ad8
Lintaaa-25 <lintababu3@gmail.com>
2025-05-24 00:17:39 +0530

    Update index.js

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,5 +1,10 @@
-const app = require("./app");
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`server listening at port - ${port}`);
+const app = require('./app');
+const PORT = 3000;
+ 
+app.listen(process.env.PORT || PORT, function(err){
+    if (err) console.log("Error in server setup")
+    console.log("Server listening on Port", PORT);
 });
+//app.listen(process.env.PORT || 3000, ()=>{
+  console.log('started express server');
+//});
