8db24411a15576975aadec7a584e79fb3df9dff1
Lintaaa-25 <lintababu3@gmail.com>
2025-05-24 15:21:27 +0530

    Update todo.js

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -1,105 +1,87 @@
 
-"use strict";
-const { Model, Op } = require("sequelize");
-
+/* eslint-disable require-jsdoc */
+'use strict';
+const {
+  Model, Op
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      Todo.belongsTo(models.User, {
-        foreignKey: "userId",
-      });
-      // define association here
+       static associate(models) {
+        Todo.belongsTo(models.User,{
+          foreignKey: 'userId'
+        })
+
+          }
+
+    static addTodo({title, dueDate, userId}) {
+      return this.create({title: title, dueDate: dueDate, completed: false,userId});
     }
-    static addTodo({ title, dueDate, userId }) {
-      return this.create({
-        title: title,
-        dueDate: dueDate,
-        completed: false,
-        userId,
-      });
-    }
-    markAsCompleted() {
-      return this.update({ completed: true });
-    }
-    deletetodo() {
-      return this.removetask(id);
-    }
-    static getTodos() {
-      return this.findAll({ order: [["id", "ASC"]] });
-    }
-    static overdue(userId) {
-      return this.findAll({
+
+    static getTodos(userId){
+  return this.findAll({ where: { userId } });
+}
+
+    static async overdue(userId) {
+  const today = new Date().toISOString().split("T")[0];
+  return await Todo.findAll({
+    where: {
+      dueDate: { [Op.lt]: today },
+      completed: false,
+      userId,
+    },
+  });
+}
+
+   static async dueToday(userId) {
+  const today = new Date().toISOString().split("T")[0];
+  return await Todo.findAll({
+    where: {
+      dueDate: { [Op.eq]: today },
+      completed: false,
+      userId,
+    },
+  });
+} 
+
+   static async dueLater(userId) {
+  const today = new Date().toISOString().split("T")[0];
+  return await Todo.findAll({
+    where: {
+      dueDate: { [Op.gt]: today },
+      completed: false,
+      userId,
+    },
+  });
+}
+
+    static async remove(id) {
+      return this.destroy({
         where: {
-          dueDate: {
-            [Op.lt]: new Date().toLocaleDateString("en-CA"),
-          },
-          userId,
-          completed: false,
+          id,
         },
-        order: [["id", "ASC"]],
-      });
+      })
     }
-    static dueToday(userId) {
-      return this.findAll({
-        where: {
-          dueDate: {
-            [Op.eq]: new Date().toLocaleDateString("en-CA"),
-          },
-          userId,
-          completed: false,
-        },
-        order: [["id", "ASC"]],
-      });
-    }
-    static dueLater(userId) {
-      return this.findAll({
-        where: {
-          dueDate: {
-            [Op.gt]: new Date().toLocaleDateString("en-CA"),
-          },
-          userId,
-          completed: false,
-        },
-        order: [["id", "ASC"]],
-      });
-    }
-    static completedItems(userId) {
+
+    static async completedItems(userId){
       return this.findAll({
         where: {
           completed: true,
           userId,
-        },
-        order: [["id", "ASC"]],
-      });
+        }
+      })
     }
-    static async remove(id, userId) {
-      return this.destroy({
-        where: {
-          id,
-          userId,
-        },
-      });
+    setCompletionStatus(receiver) {
+      return this.update({ completed: receiver });
     }
-    setCompletionStatus(bool) {
-      return this.update({ completed: bool });
-    }
+    
   }
-
-  Todo.init(
-    {
-      title: DataTypes.STRING,
-      dueDate: DataTypes.DATEONLY,
-      completed: DataTypes.BOOLEAN,
-    },
-    {
-      sequelize,
-      modelName: "Todo",
-    }
-  );
+  Todo.init({
+    title: DataTypes.STRING,
+    dueDate: DataTypes.DATEONLY,
+    completed: DataTypes.BOOLEAN,
+  }, {
+    sequelize,
+    modelName: 'Todo',
+  });
   return Todo;
 };
