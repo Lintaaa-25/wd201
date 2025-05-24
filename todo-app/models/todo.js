2d2bb1e6f5aed00ec3b9e39842b254eb8e39baf0
Lintaaa-25 <lintababu3@gmail.com>
2025-05-24 10:52:38 +0530

    Update todo.js

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -1,102 +1,105 @@
 
-/* eslint-disable require-jsdoc */
-'use strict';
-const {
-  Model, Op
-} = require('sequelize');
+"use strict";
+const { Model, Op } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
-       static associate(models) {
-        Todo.belongsTo(models.User,{
-          foreignKey: 'userId'
-        })
-
-          }
-
-    static addTodo({title, dueDate, userId}) {
-      return this.create({title: title, dueDate: dueDate, completed: false,userId});
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Todo.belongsTo(models.User, {
+        foreignKey: "userId",
+      });
+      // define association here
     }
-
-    static getTodos(userId){
-  return this.findAll({ where: { userId } });
-}
-
-    static async overdue(userId) {
-  const today = new Date().toISOString().split("T")[0];
-  return await Todo.findAll({
-    where: {
-      dueDate: { [Op.lt]: today },
-      completed: false,
-      userId,
-    },
-  });
-}
-
-   static async dueToday(userId) {
-  const today = new Date().toISOString().split("T")[0];
-  return await Todo.findAll({
-    where: {
-      dueDate: { [Op.eq]: today },
-      completed: false,
-      userId,
-    },
-  });
-} 
-
-   static async dueLater(userId) {
-  const today = new Date().toISOString().split("T")[0];
-  return await Todo.findAll({
-    where: {
-      dueDate: { [Op.gt]: today },
-      completed: false,
-      userId,
-    },
-  });
-}
-
-    static async remove(id) {
-      return this.destroy({
+    static addTodo({ title, dueDate, userId }) {
+      return this.create({
+        title: title,
+        dueDate: dueDate,
+        completed: false,
+        userId,
+      });
+    }
+    markAsCompleted() {
+      return this.update({ completed: true });
+    }
+    deletetodo() {
+      return this.removetask(id);
+    }
+    static getTodos() {
+      return this.findAll({ order: [["id", "ASC"]] });
+    }
+    static overdue(userId) {
+      return this.findAll({
         where: {
-          id,
+          dueDate: {
+            [Op.lt]: new Date().toLocaleDateString("en-CA"),
+          },
+          userId,
+          completed: false,
         },
-      })
+        order: [["id", "ASC"]],
+      });
     }
-
-    static async completedItems(userId){
+    static dueToday(userId) {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.eq]: new Date().toLocaleDateString("en-CA"),
+          },
+          userId,
+          completed: false,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+    static dueLater(userId) {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.gt]: new Date().toLocaleDateString("en-CA"),
+          },
+          userId,
+          completed: false,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+    static completedItems(userId) {
       return this.findAll({
         where: {
           completed: true,
           userId,
-        }
-      })
+        },
+        order: [["id", "ASC"]],
+      });
     }
-    setCompletionStatus(receiver) {
-      return this.update({ completed: receiver });
+    static async remove(id, userId) {
+      return this.destroy({
+        where: {
+          id,
+          userId,
+        },
+      });
     }
-    
+    setCompletionStatus(bool) {
+      return this.update({ completed: bool });
+    }
   }
-  Todo.init({
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notNull: { msg: "Title cannot be null" },
-      notEmpty: { msg: "Title cannot be empty" }
-    }
-  },
-  dueDate: {
-    type: DataTypes.DATEONLY,
-    allowNull: false,
-    validate: {
-      notNull: { msg: "Due date cannot be null" },
-      isDate: { msg: "Must be a valid date" }
-    }
-  },
-  completed: DataTypes.BOOLEAN,
-}, {
-  sequelize,
-  modelName: 'Todo',
-});
 
+  Todo.init(
+    {
+      title: DataTypes.STRING,
+      dueDate: DataTypes.DATEONLY,
+      completed: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Todo",
+    }
+  );
   return Todo;
 };
